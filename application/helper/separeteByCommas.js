6f1bcae1c116eb47d3140b653882ac8a87ca39b0
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2020-09-24 22:58:38 -0700

    I fixed some issues with the database creation scripts. I did some caching work on the types query. I finished the backend for sunbmisions(needs testing). I added some functions to separate the terms of a string by using commas.

---
+++ application/helper/separeteByCommas.js
@@ -0,0 +1,40 @@
+//This file helps with separating the tags/links of the provided string.
+//The separation is done by looking at the commas
+
+exports.separateLinks = function (nonSeparate) {
+    let separate = [];
+    let startIndex = 0;
+    let i;
+    for (i = 0; i < nonSeparate.length; i++) {
+        if (nonSeparate.charAt(i) == ',') {
+            separate.push(nonSeparate.substring(startIndex, i).replace(/ /g, ""));
+            startIndex = i + 1;
+        }
+    }
+    if (nonSeparate.charAt(i-1) != ',') {
+        separate.push(nonSeparate.substring(startIndex, i + 1).replace(/ /g, ""))
+    }
+    return separate;
+}
+
+exports.separateTags = function (nonSeparate) {
+    let separate = [];
+    let startIndex = 0;
+    if (nonSeparate.charAt(startIndex) == " ") {
+        startIndex++;
+    }
+    let i;
+    for (i = 0; i < nonSeparate.length; i++) {
+        if (nonSeparate.charAt(i) == ',') {
+            separate.push(nonSeparate.substring(startIndex, i));
+            startIndex = i + 1;
+            if (nonSeparate.charAt(startIndex) == " ") {
+                startIndex++;
+            }
+        }
+    }
+    if (nonSeparate.charAt(i-1) != ',') {
+        separate.push(nonSeparate.substring(startIndex, i + 1));
+    }
+    return separate;
+}
