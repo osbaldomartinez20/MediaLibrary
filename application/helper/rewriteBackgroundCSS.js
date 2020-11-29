9be34cf772c26bb34fe37b1bd1b21fe39295a5f1
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2020-11-28 22:00:08 -0800

    I implemented an admin function to change background color.

--- application/helper/rewriteBackgroundCSS.js
+++ application/helper/rewriteBackgroundCSS.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 exports.writeBackgroundColorCSS = (color, location, file) => {
-    let cssToWrite = location + ' {\nbackground-color: " + color + ";\n}';
+    let cssToWrite = location + ' {\n\tbackground-color:' + color + ';\n}';
     fs.unlink('./public/stylesheets/' + file, (err) => {
         if(err) {
             fs.writeFileSync(__dirname + '/errors/' + Date.now() + 'error.log', err + '');
+            return;
         }
         fs.writeFileSync('./public/stylesheets/' + file, cssToWrite);
     });
