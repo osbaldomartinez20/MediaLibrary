6c558153ab2804b8daeb2efedaba98a00dfb3247
osbaldomartinez20 <osbaldomartineziniguez@gmail.com>
2021-01-17 15:03:02 -0800

    I added the code to show the content of different pages of the results.

--- application/public/js/pagination.js
+++ application/public/js/pagination.js
@@ -6,34 +6,25 @@ const ENTRIES_PER_PAGE = 10;
 
 let makePagination = () => {
     let docSelection = document.getElementsByClassName("resultRow");
-    let filtered = checkFilter();
     let pages = Math.ceil(docSelection.length / ENTRIES_PER_PAGE);
 
-    if (filtered) {
-        for (let i = 0; i < docSelection.length; i++) {
-            let temp = docSelection[i].getElementsByTagName("TD")[3].childNodes[0].innerText;
-            if (!document.getElementById(temp).checked || i >= ENTRIES_PER_PAGE) {
-                docSelection[i].classList.add("hiddenP");
-            }
-        }
-    } else {
-        for (let i = 0; i < docSelection.length; i++) {
-            if(i >= ENTRIES_PER_PAGE) {
-                docSelection[i].classList.add("hiddenP");
-            }
+    for (let i = 0; i < docSelection.length; i++) {
+        if (i >= ENTRIES_PER_PAGE) {
+            docSelection[i].classList.add("hiddenP");
         }
     }
 
-    if(pages > 1) {
+
+    if (pages > 1) {
         for (let i = 0; i < pages; i++) {
 
             let a = document.createElement('a');
             a.appendChild(document.createTextNode((i + 1) + ""));
-            a.title = (i+1) + "";
+            a.title = (i + 1) + "";
             a.href = "#"
-            a.onclick = showResultsInPage(i+1);
+            a.onclick = showResultsInPage(i + 1);
 
-            if(i == 0) {
+            if (i == 0) {
                 a.classList.add("active");
             }
             document.getElementById("paginationBar").appendChild(a);
@@ -41,15 +32,19 @@ let makePagination = () => {
     }
 }
 
-let checkFilter = () => {
-    for (let i = 0; i < tsf.types.length; i++) {
-        if (document.getElementById(tsf.types[i]).checked) {
-            return true;
-        }
-    }
-    return false;
-}
 
 let showResultsInPage = (page) => {
+    let max = page * 10;
+    let min = (page * 10) - 10;
 
+    let docSelection = document.getElementsByClassName("resultRow");
+
+    for(let i = 0; i < docSelection.length; i++) {
+        if(i >= min || i < max) {
+            docSelection[i].classList.remove("hiddenP");
+        } else {
+            docSelection[i].classList.add("hiddenP");
+        }
+    }
+    
 }
