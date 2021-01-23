c7cbf517cf7bbd00448a65be30b116d682acbf7f
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2021-01-22 22:12:19 -0800

    Pagination works on develop enviroment. Change the makePagibation into jQuery and added pagination div after table.

--- application/public/js/pagination.js
+++ application/public/js/pagination.js
@@ -1,10 +1,36 @@
 //This file helps with the pagination of the results
-const tsf = require("./filterByTypes");
 
 //This determines the number of entries per page.
 const ENTRIES_PER_PAGE = 10;
 
-let makePagination = () => {
+let showResultsInPage = (page, pages) => {
+    let max = page * 10;
+    let min = (page * 10) - 10;
+
+    let docSelection = document.getElementsByClassName("resultRow");
+
+    for(let i = 0; i < docSelection.length; i++) {
+        if(i >= min && i < max) {
+            docSelection[i].classList.remove("hiddenP");
+            
+        } else {
+            docSelection[i].classList.add("hiddenP");
+        }
+    }
+
+    if(pages > 1) {
+        docSelection = document.getElementsByClassName("pageNumber");
+        for(let i = 0; i < docSelection.length; i++) {
+            if(page - 1 == i) {
+                docSelection[i].classList.add("active");
+            } else {
+                docSelection[i].classList.remove("active");
+            }
+        }
+    }
+}
+
+$(document).ready(function() {
     let docSelection = document.getElementsByClassName("resultRow");
     let pages = Math.ceil(docSelection.length / ENTRIES_PER_PAGE);
 
@@ -21,8 +47,9 @@ let makePagination = () => {
             let a = document.createElement('a');
             a.appendChild(document.createTextNode((i + 1) + ""));
             a.title = (i + 1) + "";
+            a.classList.add("pageNumber");
             a.href = "#"
-            a.onclick = showResultsInPage(i + 1);
+            a.setAttribute("onclick", "showResultsInPage(" + (i + 1) + "," + pages + ")")
 
             if (i == 0) {
                 a.classList.add("active");
@@ -30,21 +57,4 @@ let makePagination = () => {
             document.getElementById("paginationBar").appendChild(a);
         }
     }
-}
-
-
-let showResultsInPage = (page) => {
-    let max = page * 10;
-    let min = (page * 10) - 10;
-
-    let docSelection = document.getElementsByClassName("resultRow");
-
-    for(let i = 0; i < docSelection.length; i++) {
-        if(i >= min || i < max) {
-            docSelection[i].classList.remove("hiddenP");
-        } else {
-            docSelection[i].classList.add("hiddenP");
-        }
-    }
-    
-}
+});
