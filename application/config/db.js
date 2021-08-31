52cee62eec21318916428f5ba480c35828834980
osbaldomartinez20 <osbaldomartineziniguez@gmail.com>
2021-08-31 00:21:26 +0000

    fixed database problems.

--- application/config/db.js
+++ application/config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
+    user: "oz",
     password: "2020WebApp",
     database: "tsfc",
     multipleStatements: true,
